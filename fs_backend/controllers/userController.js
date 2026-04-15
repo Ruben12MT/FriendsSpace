910c4c0df9a16ad2475b77d639a683868be459e1
Ruben12MT <xrmartri675@ieshnosmachado.org>
2026-04-16 00:06:37 +0200

    Posible correccion

--- fs_backend/controllers/userController.js
+++ fs_backend/controllers/userController.js
@@ -13,8 +13,8 @@ const generarToken = (usuario) => jwt.sign(
 
 const cookieOpts = {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   maxAge: 1000 * 60 * 60,
 };
 
